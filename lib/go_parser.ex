65fa9597af36049ce3c57a7058a74847ce222867
koko le gorille <koko.le.gorille@gmail.com>
2017-04-29 18:57:05 +0200

    Initial commit

---
+++ lib/go_parser.ex
@@ -0,0 +1,18 @@
+defmodule GoParser do
+  @moduledoc """
+  Documentation for GoParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GoParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
