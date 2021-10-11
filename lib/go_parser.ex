50f73022e3aae8a3dcf2cecb59297e79c487c4c4
mr5iff <mr5iff@gmail.com>
2021-10-11 23:31:23 +0800

    add strip_utf to remove non utf characters

--- lib/go_parser.ex
+++ lib/go_parser.ex
@@ -37,10 +37,34 @@ defmodule GoParser do
       any -> any
     end
   end
-
   defp lex_string(string) do
     string
+    |> Strip.strip_utf
     |> to_charlist
     |> :sgf_lexer.string
   end
 end
+
+
+defmodule Strip do
+  @doc """
+    iex> Strip.strip_utf "Tallak\xc3\xb1 Tveide"
+    "Tallakñ Tveide"
+  """
+  def strip_utf(str) do
+    strip_utf_helper(str, [])
+  end
+
+  defp strip_utf_helper(<<x :: utf8>> <> rest, acc) do
+    strip_utf_helper rest, [x | acc]
+  end
+
+  defp strip_utf_helper(<<x>> <> rest, acc), do: strip_utf_helper(rest, acc)
+
+  defp strip_utf_helper("", acc) do
+    acc
+    |> :lists.reverse
+    |> List.to_string
+  end
+end
+
